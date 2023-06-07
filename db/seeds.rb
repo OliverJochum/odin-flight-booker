bc6f05d4b2d2d16c94e699ca38b8b5a1bcaf877f
Oliver Jochum <oliver.jochum@yahoo.com>
2023-06-07 15:42:59 +0200

    populated db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+airports = Airport.create([{code: "FRA",name:"Frankfurt"},{code: "CPH", name:"Copenhagen"},{code:"JFK",name:"John F. Kennedy"},{code:"LBA",name:"Leeds Bradford"}])
+
+flights = Flight.create([{departure_airport_id:"FRA", arrival_airport_id:"CPH",start:DateTime.new(2023, 6, 7, 10, 30, 0),duration:2},{departure_airport_id:"JFK", arrival_airport_id:"LBA",start:DateTime.new(2023, 6, 12, 13, 0, 0),duration:9}])
+
