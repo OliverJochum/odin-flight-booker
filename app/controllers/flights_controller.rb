6a8939c6892f49cfc8377aadc5e2e005ef73a76c
Oliver Jochum <oliver.jochum@yahoo.com>
2023-06-07 16:54:32 +0200

    created search form in index

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,9 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all
+    @airport_options = Airport.all.map{|a| [a.code]}
+    @start_options = Flight.all.map{|f| [f.start]}
+
+
+    @flights = Flight.search params[:departure_airport_id], params[:arrival_airport_id], params[:start].to_datetime
   end
 end
