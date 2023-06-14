9afefe79dfbbfd8905799b8a07b32742bc3ee271
Oliver Jochum <oliver.jochum@yahoo.com>
2023-06-14 17:46:28 +0200

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,15 @@ Rails.application.routes.draw do
   # root "articles#index"
   root "flights#index"
 
+  resources :bookings do
+    resources :flights
+    resources :passengers
+  end
+
+  resources :passengers do
+    resources :flights
+  end
+
   resources :airports do
     resources :flights
   end
