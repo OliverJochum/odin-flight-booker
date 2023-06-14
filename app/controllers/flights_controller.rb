172e19852a2b4597007640cbcc9fced41dc2296f
Oliver Jochum <oliver.jochum@yahoo.com>
2023-06-14 17:46:21 +0200

    fix for nullpointer

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,7 +3,9 @@ class FlightsController < ApplicationController
     @airport_options = Airport.all.map{|a| [a.code]}
     @start_options = Flight.all.map{|f| [f.start]}
 
-
-    @flights = Flight.search params[:departure_airport_id], params[:arrival_airport_id], params[:start].to_datetime
+    @flights = []
+    if params[:departure_airport_id] and params[:arrival_airport_id] and params[:start]
+      @flights = Flight.search params[:departure_airport_id], params[:arrival_airport_id], params[:start].to_datetime 
+    end
   end
 end
