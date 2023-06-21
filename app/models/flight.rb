597c2d67d4c823444d1cebdc07c57008a07a6f77
Oliver Jochum <oliver.jochum@yahoo.com>
2023-06-21 17:52:28 +0200

    Added booking & passenger associations

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,9 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport', foreign_key: :departure_airport_id, primary_key: :code
   belongs_to :arrival_airport, class_name: 'Airport', foreign_key: :arrival_airport_id, primary_key: :code
+  belongs_to :booking
+  belongs_to :passenger
+
 
   validates :departure_airport_id, length: {is: 3}
   validates :arrival_airport_id, length: {is: 3}
