ac00bc8fa3345320a3a1ba02e19069dbb0c08e75
Oliver Jochum <oliver.jochum@yahoo.com>
2023-06-21 17:52:06 +0200

    Created

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,27 @@
 class BookingsController < ApplicationController
   def new
+    @flight_id = params[:flight_id]
+    @number_passengers = params[:number_passengers]
 
+    @flight = Flight.find(@flight_id)
+
+    @booking = Booking.new
+
+    @passengers = []
+
+    for i in 0..number_passengers-1
+      passenger = Passenger.new
+      @passengers.push(passenger)
+    end
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:id, :name, :email])
   end
 end
