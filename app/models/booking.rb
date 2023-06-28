4ceffacb7a12966813bd51e33b23d391981a1f8b
Oliver Jochum <oliver.jochum@yahoo.com>
2023-06-28 17:54:28 +0200

    added accepts_nested_attributes for

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
-  has_one :flights
+  has_one :flight, foreign_key: :flight_id
   has_many :passengers
+
+  accepts_nested_attributes_for :flight, :passengers
 end
