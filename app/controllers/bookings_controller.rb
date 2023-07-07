e71fe2ba4264dcd9f7f26ba62c95b21885773d0f
Oliver Jochum <oliver.jochum@yahoo.com>
2023-07-07 17:12:15 +0200

    fixed new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,26 +6,33 @@ class BookingsController < ApplicationController
     @flight = Flight.find(@flight_id)
 
     @booking = Booking.new
+    @booking.passengers.build
 
-    @passengers = []
+    # @passengers = []
 
-    for i in 0..(@number_passengers.to_i - 1)
-      passenger = Passenger.new
-      @passengers.push(passenger)
-    end
+    # for i in 0..(@number_passengers.to_i - 1)
+    #   passenger = Passenger.new
+    #   @passengers.push(passenger)
+    # end
   end
 
   def create
+
     @booking = Booking.new(booking_params)
 
-    if @booking.save
-      flash[:alert] = "Success"
+    if @booking.save!
+      redirect_to @booking
+    else
+      render :new, status: :unprocessable_entity 
     end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
   end
 
   private
   def booking_params
-    params.require(:booking).permit(:flight_id, passengers_attributes: [:id, :name, :email])
-    params.permit(:start, :departure_airport_id, :arrival_airport_id)
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email, :flight_id])
   end
 end
